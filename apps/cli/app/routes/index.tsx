333d2d27d9e37390e2e7a9612142d46304b56b77
Oleksandr Havrylov <sashka.havr@gmail.com>
2025-03-25 00:41:49 +0100

    Init

---
+++ apps/cli/app/routes/index.tsx
@@ -0,0 +1,17 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+function get_rand() {
+  return Math.random() > 0.5 ? 1 : undefined;
+}
+
+export const Route = createFileRoute('/')({
+  component: Home,
+});
+
+function Home() {
+  if (get_rand()) {
+    return 'Works!';
+  }
+
+  return 'Works!';
+}
