8f169a020dcd49892b9e9b520ea60b42a318a0cd
Oleksandr Havrylov <sashka.havr@gmail.com>
2025-03-25 23:04:15 +0100

    Remove hono

--- apps/web/app/apiClient.tsx
+++
@@ -1,5 +0,0 @@
-import { hc } from 'hono/client';
-
-import type { AppType } from '@where-are-my-games/api';
-
-export const api = hc<AppType>('http://localhost:5000');
--- packages/api/src/routes/hello.ts
+++
@@ -1,5 +0,0 @@
-import { Hono } from 'hono';
-
-export const hello = new Hono().get('', (c) => {
-  return c.text('Hello Hono!');
-});
