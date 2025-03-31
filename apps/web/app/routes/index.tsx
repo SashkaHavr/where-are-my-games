261e5f60da180099496abd3f9eef4b896701723c
Oleksandr Havrylov <sashka.havr@gmail.com>
2025-03-31 20:12:30 +0200

    Add env

--- apps/web/app/routes/index.tsx
+++ apps/web/app/routes/index.tsx
@@ -6,18 +6,20 @@ import { Button } from '@where-are-my-games/ui-web/ui/button.tsx';
 
 export const Route = createFileRoute('/')({
   component: Home,
-  loader: async ({ context }) => await context.trpc.hello.query(),
+  loader: async ({ context }) => ({
+    hello: await context.trpc.hello.query(),
+  }),
 });
 
 function Home() {
   const trpc = useTRPC();
   const useHello = useQuery(trpc.hello.queryOptions());
 
-  const state = Route.useLoaderData();
+  const data = Route.useLoaderData();
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <p>Works!</p>
-      <p>{state}</p>
+      <p>{data.hello}</p>
       {useHello.isSuccess && <p>On client: {useHello.data}</p>}
       <Button variant="default">This is a button</Button>
     </div>
